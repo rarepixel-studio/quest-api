4b297220950a3eebc6d43c7f94a87a663bd4a9ad
hossein <jerzi.net@gmail.com>
2016-01-16 14:28:07 +0330

    validate response

--- src/QuestionnaireAPI.php
+++ src/QuestionnaireAPI.php
@@ -6,10 +6,16 @@ class QuestionnaireAPI extends HttpClient
 {
     public function createAnswerSheet($questionnaireId, $pushBackUrl = null)
     {
-        return $this->makeRequest('post', 'create-answersheet', [
+        $response = $this->makeRequest('post', 'create-answersheet', [
             'questionnaire_id' => $questionnaireId,
             'push_back_url'    => $pushBackUrl,
         ]);
+
+        if (array_keys($response) == ['status', 'answersheet_id', 'link'] && $response['status'] == 'success') {
+            return $response;
+        } else {
+            throw new \Exception('Invalid response for questionnaire[' . $questionnaireId . ']');
+        }
     }
 
     public function sayHi()
